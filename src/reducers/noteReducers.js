28794d3c803613c7e264a322e16773a714fda9c0
Gamy <flokygamy@gmail.com>
2021-12-04 02:58:58 +0000

    added id field to each item

--- src/reducers/noteReducers.js
+++ src/reducers/noteReducers.js
@@ -1,15 +1,17 @@
 
 const initialState = {
     notes : [
-     {    
+     {   id:1,
          title : "Geography",
          Note : " Geography is a subject that talks about countries and landmarks"
      },
      {   
+         id:2,
          title : "Science",
          Note : " Science is all about science"
      },
      {    
+         id:3,
          title : "English",
          Note : " English  is an awesome language but not then my language"
      }
@@ -22,11 +24,14 @@ const noteReducer = ( state = initialState , action ) =>{
          return { ...state , notes:[...state.notes, action.payload]};
          case "DELETE_NOTE":
              const deletedNote = state.notes.filter((note)=>{
-                 if( note.id !== action.payload){
-                     return note;
-                 }
+                return  note.id ? note.id !== action.payload : note
              })
-          return {...state, notes: deletedNote}
+             return { ...state, notes: deletedNote }
+         case "EDIT":
+             const editedNotes = state.notes.map((note) => {
+                 return note.id === action.payload.noteId ? action.payload.editedNote : note
+             })
+             return { ...state, notes: editedNotes };
 
           default:
             return state ;
