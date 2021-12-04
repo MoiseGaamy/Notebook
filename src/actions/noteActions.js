30e8434c1d8572043c9fb84a1e23f4cf1807118a
Gamy <flokygamy@gmail.com>
2021-12-04 02:56:10 +0000

    implemented noteActions

--- src/actions/noteActions.js
+++ src/actions/noteActions.js
@@ -4,9 +4,15 @@ export const  addNote = (note) => {
          payload: note
      }
 }
-export const  deleteNote = (noteId) => {
+export const deleteNote = (noteId) => {
     return {
         type: "DELETE_NOTE",
         payload: noteId
     };
-}
+};
+export const editNote = (noteId, editedNote) => {
+    return {
+        type: "EDIT",
+        payload: { noteId, editedNote }
+    };
+};
